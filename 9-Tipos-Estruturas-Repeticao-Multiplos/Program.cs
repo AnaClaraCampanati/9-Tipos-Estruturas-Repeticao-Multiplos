31c35f407545011c63ac3afbf7828af3b1e4f804
Ana Clara campanati <campanatianaclara@gmail.com>
2023-01-06 09:22:25 -0300

    Adicionar arquivos de projeto.

---
+++ 9-Tipos-Estruturas-Repeticao-Multiplos/Program.cs
@@ -0,0 +1,14 @@
+﻿// Faça um programa em C# que imprima todos os múltiplos de 3, entre 1 e 100.
+// Para saber se um número é múltiplo de 3, você pode fazer if(numero % 3 == 0).
+
+int multiplo3 = 0;
+
+for (int i = 1; i <= 21; i++)
+{
+    if (i % 3 == 0)
+    {
+        multiplo3 = multiplo3 + 1;
+    }
+ }
+
+Console.WriteLine("Os números multiplos por 3 são: " + multiplo3);
